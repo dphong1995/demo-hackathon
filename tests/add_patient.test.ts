484f2e00a94ccaae511a6ac3eb1ff2761ba635e0
Phong <pluu@talosix.com>
2025-03-26 14:38:03 +0700

    add patient

--- tests/add_patient.test.ts
+++ tests/add_patient.test.ts
@@ -1,8 +1,16 @@
 import { test } from "./fixture";
+import { faker } from "@faker-js/faker";
 
 const TALOSIX_APP_URL = process.env.TALOSIX_APP_URL;
 
-test("add patient", async ({ aiTap, aiWaitFor, aiAssert, page }) => {
+test("add patient", async ({
+  aiTap,
+  aiWaitFor,
+  aiQuery,
+  page,
+  aiInput,
+  aiAssert,
+}) => {
   await page.goto(TALOSIX_APP_URL);
   await aiTap("Patient Management menu item in the left sidebar");
   await aiTap("Patient Summary menu item");
@@ -14,5 +22,28 @@ test("add patient", async ({ aiTap, aiWaitFor, aiAssert, page }) => {
     timeoutMs: 20000,
   });
 
-  await aiAssert("Patient Registration form is visible");
+  await aiTap("Site selection input in the registration form");
+  await aiTap(
+    "Random site option from the site selection dropdown in the registration form",
+  );
+  await page.waitForTimeout(3000);
+  const patientId = await aiQuery("Generated patient ID");
+  const patientEmail = faker.internet.exampleEmail();
+  await aiInput(patientEmail, "Primary Email in the registration form");
+  await aiTap("Submit and close in the registration form");
+  await page.waitForTimeout(3000);
+  await aiInput(
+    patientId,
+    "Patient ID search on top of the table in the patient summary page",
+  );
+  await aiTap("Search button on top of the table in the patient summary page");
+  await aiWaitFor(
+    `${patientId} exists in the patient table in the patient summary page`,
+    {
+      timeoutMs: 20000,
+    },
+  );
+  await aiAssert(
+    `${patientId} exists in the patient table in the patient summary page`,
+  );
 });
